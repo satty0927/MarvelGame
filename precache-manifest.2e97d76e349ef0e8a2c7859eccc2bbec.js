c659d6dac9ca42d95446b9812773f9d8743f730a
satyam323545 <53506206+satyam323545@users.noreply.github.com>
2020-08-03 16:04:08 +0530

    Updates

---
+++ precache-manifest.2e97d76e349ef0e8a2c7859eccc2bbec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e958ece61168f62aa893217ee857ecaf",
+    "url": "/MarvelGame/index.html"
+  },
+  {
+    "revision": "b0fb7d2dc3de329defaf",
+    "url": "/MarvelGame/static/css/main.27747e7e.chunk.css"
+  },
+  {
+    "revision": "0fdaae023372bfa9a5ef",
+    "url": "/MarvelGame/static/js/2.26340233.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MarvelGame/static/js/2.26340233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0fb7d2dc3de329defaf",
+    "url": "/MarvelGame/static/js/main.ae195b2e.chunk.js"
+  },
+  {
+    "revision": "97f71334aeb30ec09089",
+    "url": "/MarvelGame/static/js/runtime-main.f0b4eac0.js"
+  }
+]);
